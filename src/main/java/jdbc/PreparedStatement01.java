b53bca65e2999110476389734325aa00d408ea08
Fethi <fethiguneyy@gmail.com>
2022-09-01 23:24:57 +0300

    01/9/22

---
+++ src/main/java/jdbc/PreparedStatement01.java
@@ -0,0 +1,78 @@
+package jdbc;
+
+import java.sql.*;
+
+public class PreparedStatement01 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+
+        Connection con= DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed", "postgres", "274817");
+
+        Statement st= con.createStatement();
+
+//1. Örnek: Prepared statement kullanarak company adı IBM
+// olan number_of_employees değerini 9999 olarak güncelleyin.
+
+    //1.adım : Prepared statement query'sini olustur
+    String sql1="UPDATE companies SET number_of_employees = ? WHERE company= ? ";
+
+    //2.adım : PreparedStatement objesini olustur
+    PreparedStatement pst1=con.prepareStatement(sql1);
+
+    //3.adım : set () methodlari ile soru işaretleri için deger girilir
+        pst1.setInt(1,9999);
+        pst1.setString(2, "IBM");
+
+    //4.adım :Execute Query
+    int updateRowNumbers=pst1.executeUpdate();
+        System.out.println(updateRowNumbers+" satir guncellendi");
+
+        String sql="SELECT * from companies";
+        ResultSet result1=st.executeQuery(sql);
+
+        while (result1.next()) {
+            System.out.println(result1.getInt(1) +
+                    "-" + result1.getString(2) +
+                    "-" + result1.getInt(3));
+        }
+
+
+
+        pst1.setInt(1,15000);
+        pst1.setString(2, "GOOGLE");
+
+        //4.adım :Execute Query
+        int updateRowNumbers1=pst1.executeUpdate();
+        System.out.println(updateRowNumbers+" satir guncellendi");
+
+        String sql2="SELECT * from companies";
+        ResultSet result2=st.executeQuery(sql2);
+
+        while (result2.next()) {
+            System.out.println(result2.getInt(1) +
+                    "-" + result2.getString(2) +
+                    "-" + result2.getInt(3));
+        }
+
+//2. Örnek: "SELECT * FROM <table name>" query'sini
+// prepared statement ile kullanın.
+
+    String sql3="SELECT * from ? ";
+    PreparedStatement pst2=con.prepareStatement(sql3);
+    pst2.setString(1, "companies");
+
+    ResultSet result4=pst2.executeQuery(sql3);
+    while (result4.next()) {
+        System.out.println(result4.getInt(1) +
+                "-" + result4.getString(2) +
+                "-" + result4.getInt(3));
+    }
+
+
+
+
+
+    }
+}
